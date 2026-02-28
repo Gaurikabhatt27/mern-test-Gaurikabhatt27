013d39abbd48422fb06047490a402b11d2696ba7
Gaurika Bhatt <gaurikabhatt27@gmail.com>
2026-02-28 08:42:34 +0530

    feat: Improve auth UI and configure backend connection

--- CMS_BACKEND/server.js
+++ CMS_BACKEND/server.js
@@ -1,7 +1,22 @@
 import express from "express"
 import http from "http";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+connectDB();
 
 const app = express();
+app.use(cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true
+}));
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 8080;
